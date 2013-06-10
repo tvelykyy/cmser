c9a9736d13ea5d3aa6be01780b8b689961b52407
Taras <tavelyky@gmail.com>
2013-06-10 16:25:36 +0300

    DB script update.

--- application/resources/init_db.sql
+++ application/resources/init_db.sql
@@ -10,19 +10,19 @@ Target Server Type    : MYSQL
 Target Server Version : 50531
 File Encoding         : 65001
 
-Date: 2013-05-28 22:32:47
+Date: 2013-06-10 16:21:16
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for page
+-- Table structure for `page`
 -- ----------------------------
 DROP TABLE IF EXISTS `page`;
 CREATE TABLE `page` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Represent unique page identifier',
   `parent_id` bigint(20) NOT NULL,
-  `url` varchar(200) NOT NULL COMMENT 'Represents page''s full URL.',
+  `uri` varchar(200) NOT NULL COMMENT 'Represents page''s full URL.',
   `template_id` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
@@ -34,7 +34,7 @@ INSERT INTO `page` VALUES ('1', '0', '/', '1');
 INSERT INTO `page` VALUES ('2', '1', '/news', '1');
 
 -- ----------------------------
--- Table structure for page_field
+-- Table structure for `page_field`
 -- ----------------------------
 DROP TABLE IF EXISTS `page_field`;
 CREATE TABLE `page_field` (
@@ -51,13 +51,13 @@ INSERT INTO `page_field` VALUES ('2', 'HEADER');
 INSERT INTO `page_field` VALUES ('3', 'FOOTER');
 
 -- ----------------------------
--- Table structure for page_page_field
+-- Table structure for `page_page_field`
 -- ----------------------------
 DROP TABLE IF EXISTS `page_page_field`;
 CREATE TABLE `page_page_field` (
   `page_id` bigint(20) unsigned NOT NULL COMMENT 'Represent page identifier.',
   `page_field_id` bigint(20) unsigned NOT NULL COMMENT 'Represents page''s block identifier',
-  `page_field_content` blob NOT NULL COMMENT 'Reprents page block content for specific page.',
+  `page_field_content` text NOT NULL COMMENT 'Reprents page block content for specific page.',
   UNIQUE KEY `page_id_page_block_id` (`page_id`,`page_field_id`) USING BTREE COMMENT 'Page and page block combination should be unique.',
   KEY `page_field_id` (`page_field_id`),
   CONSTRAINT `page_field_id` FOREIGN KEY (`page_field_id`) REFERENCES `page_field` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -67,11 +67,11 @@ CREATE TABLE `page_page_field` (
 -- ----------------------------
 -- Records of page_page_field
 -- ----------------------------
-INSERT INTO `page_page_field` VALUES ('1', '1', 0x54686973206973207061676520636F6E74656E7420636F6E7461696E656420696E20434F4E54454E5420626C6F636B2E);
-INSERT INTO `page_page_field` VALUES ('2', '1', 0x54686973206973206E65777320706167652E);
+INSERT INTO `page_page_field` VALUES ('1', '1', 'This is page content contained in CONTENT block.');
+INSERT INTO `page_page_field` VALUES ('2', '1', 'This is news page.');
 
 -- ----------------------------
--- Table structure for template
+-- Table structure for `template`
 -- ----------------------------
 DROP TABLE IF EXISTS `template`;
 CREATE TABLE `template` (
