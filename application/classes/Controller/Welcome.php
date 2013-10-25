2c726fb59c2e57d956b5354de3800df8fa717d26
Taras Velykyy <tvelykyy@llnw.com>
2013-10-25 17:55:20 +0300

    Refactoring: created Helper_Array class.

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -11,7 +11,7 @@ class Controller_Welcome extends Controller
         if ($page)
         {
             Resolver_Snippet::resolve($page->fields);
-            $page->fields = self::convert_field_array($page->fields);
+            $page->fields = Helper_Array::convert_array_to_associative_array_for_page($page->fields);
 
             echo Generator_Html::generate_html_by_filepath_and_params($page->filepath, $page->fields);
         }
@@ -21,29 +21,4 @@ class Controller_Welcome extends Controller
         }
     }
 
-    /**
-     * Converts example array
-     * Array ( [0] => stdClass Object ( [title] => MAIN_CONTENT [page_field_content] => some text. ) )
-     * to
-     * Array ( [MAIN_CONTENT] = > some text.
-     */
-    /* TODO move this out. */
-    private static function convert_field_array($fields_array)
-    {
-        if (is_array($fields_array))
-        {
-            $result_array = array();
-
-            foreach ($fields_array as $field)
-            {
-                if (isset($field->title))
-                {
-                    $result_array[$field->title] = $field->page_field_content;
-                }
-            }
-            return $result_array;
-        }
-        return NULL;
-    }
-   
 } // End Welcome
---
+++ application/classes/Helper/Array.php
@@ -0,0 +1,28 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Helper_Array extends Controller
+{
+    /**
+     * Converts example array
+     * Array ( [0] => stdClass Object ( [title] => MAIN_CONTENT [page_field_content] => some text. ) )
+     * to
+     * Array ( [MAIN_CONTENT] = > some text.
+     */
+    public static function convert_array_to_associative_array_for_page($fields_array)
+    {
+        if (is_array($fields_array))
+        {
+            $result_array = array();
+
+            foreach ($fields_array as $field)
+            {
+                if (isset($field->title))
+                {
+                    $result_array[$field->title] = $field->page_field_content;
+                }
+            }
+            return $result_array;
+        }
+        return NULL;
+    }
+} // End Helper_Array
