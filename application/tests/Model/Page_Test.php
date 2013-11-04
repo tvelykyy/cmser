5fdbba5d1af46ab09be4996555e7624afc476777
Taras Velykyy <tavelyky@gmail.com>
2013-11-04 23:42:21 +0200

    Continuing with unit tests for models.

--- application/tests/Model/Page_Test.php
+++ application/tests/Model/Page_Test.php
@@ -12,9 +12,11 @@ class Model_Page_Test extends Model_Base_Test
 
     public function test_get_pages_with_default_parameters()
     {
+        /* When. */
         $pages = $this->model->get_pages();
         $the_only_page = $pages[0];
 
+        /* Then. */
         $this->assertNotEmpty($pages);
         $this->assertEquals(1, count($pages));
         $this->assertEquals(1, $the_only_page->id);
@@ -25,9 +27,11 @@ class Model_Page_Test extends Model_Base_Test
 
     public function test_get_pages_get_page_with_offset_one()
     {
+        /* When. */
         $pages = $this->model->get_pages(1, 1);
         $the_only_page = $pages[0];
 
+        /* Then. */
         $this->assertNotEmpty($pages);
         $this->assertEquals(1, count($pages));
         $this->assertEquals(2, $the_only_page->id);
@@ -38,9 +42,38 @@ class Model_Page_Test extends Model_Base_Test
 
     public function test_get_pages_with_limit_two()
     {
+        /* When. */
         $pages = $this->model->get_pages(0, 2);
 
+        /* Then. */
         $this->assertNotEmpty($pages);
         $this->assertEquals(2, count($pages));
+    }
+
+    public function test_get_page_by_uri_get_page_with_full_info()
+    {
+        /* When. */
+        $page = $this->model->get_page_by_uri('/news');
+
+        /* Then. */
+        $this->assertNotNull($page);
+        $this->assertEquals(2, $page->id);
+        $this->assertEquals('index.html', $page->filepath);
+        $this->assertEquals(3, count($page->fields));
+        $this->assertEquals('MAIN_CONTENT', $page->fields[0]->title);
+        $this->assertEquals('This is news page.', $page->fields[0]->page_field_content);
+        $this->assertEquals('HEADER', $page->fields[1]->title);
+        $this->assertEquals('This is news page title.', $page->fields[1]->page_field_content);
+        $this->assertEquals('FOOTER', $page->fields[2]->title);
+        $this->assertEquals('This is news page footer.', $page->fields[2]->page_field_content);
+    }
+
+    public function test_get_page_by_uri_with_no_existent_page()
+    {
+        /* When. */
+        $page = $this->model->get_page_by_uri('/non-existent-page');
+
+        /* Then. */
+        $this->assertFalse($page);
     }
 }
---
+++ application/tests/Model/Template_Test.php
@@ -0,0 +1,34 @@
+<?php defined('SYSPATH') or die('No direct access allowed!');
+
+class Model_Template_Test extends Model_Base_Test
+{
+    protected $model;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->model = new Model_Template('test');
+    }
+
+    public function test_get_template_by_id_for_existent_template()
+    {
+        /* When. */
+        $template = $this->model->get_template_by_id(1);
+
+        /* Then. */
+        $this->assertNotEmpty($template);
+        $this->assertEquals(1, $template->id);
+        $this->assertEquals('Main Template', $template->title);
+        $this->assertEquals('index.html', $template->filepath);
+    }
+
+    public function test_get_template_by_id_for_non_existent_template()
+    {
+        /* When. */
+        $template = $this->model->get_template_by_id(0);
+
+        /* Then. */
+        $this->assertFalse($template);
+    }
+
+}
