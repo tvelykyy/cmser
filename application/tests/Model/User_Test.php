2a6d2ce5fd399f486fc4da0d373096cddfa7d934
Taras Velykyy <tvelykyy@llnw.com>
2013-11-20 17:11:33 +0200

    Minor Refactoring of tests.

--- application/tests/Model/User_Test.php
+++ application/tests/Model/User_Test.php
@@ -4,6 +4,11 @@ class Model_User_Test extends Model_Base_Test
 {
     protected $model;
 
+    const EMAIL = 'dummy@mail.com';
+    const HASHED_PASSWORD = '0f0a9a777952ceb5b629ec5a901df612c7bf2cd66a63ef2d80228d5557ca8dca';
+    const USER_NAME = 'Dummy';
+    const ABRA_CADABRA_STRING = 'non-existent';
+
     public function setUp()
     {
         parent::setUp();
@@ -13,21 +18,17 @@ class Model_User_Test extends Model_Base_Test
     public function test_get_enabled_user_by_email_and_hashed_password()
     {
         /* When. */
-        $user = $this->model->get_enabled_user_by_email_and_hashed_password('dummy@mail.com',
-            '0f0a9a777952ceb5b629ec5a901df612c7bf2cd66a63ef2d80228d5557ca8dca');
+        $user = $this->model->get_enabled_user_by_email_and_hashed_password(self::EMAIL, self::HASHED_PASSWORD);
 
         /* Then. */
-        $this->assertNotEmpty($user);
-        $this->assertEquals('dummy@mail.com', $user->email);
-        $this->assertEquals('0f0a9a777952ceb5b629ec5a901df612c7bf2cd66a63ef2d80228d5557ca8dca', $user->password);
-        $this->assertTrue($user->enabled == true);
-        $this->assertEquals('Dummy', $user->username);
+        $this->assertDummyUser($user);
     }
 
     public function test_get_enabled_user_by_email_and_hashed_password_for_non_existent_user()
     {
         /* When. */
-        $user = $this->model->get_enabled_user_by_email_and_hashed_password('non-existent', 'non_existent');
+        $user = $this->model->get_enabled_user_by_email_and_hashed_password(self::ABRA_CADABRA_STRING,
+            self::ABRA_CADABRA_STRING);
 
         /* Then. */
         $this->assertEmpty($user);
@@ -36,7 +37,7 @@ class Model_User_Test extends Model_Base_Test
     public function test_get_user_by_email()
     {
         /* When. */
-        $user = $this->model->get_user_by_email('dummy@mail.com');
+        $user = $this->model->get_user_by_email(self::EMAIL);
 
         /* Then. */
         $this->assertDummyUser($user);
@@ -45,7 +46,7 @@ class Model_User_Test extends Model_Base_Test
     public function test_getuser_by_email_for_non_existent_user()
     {
         /* When. */
-        $user = $this->model->get_user_by_email('non-existent');
+        $user = $this->model->get_user_by_email(self::ABRA_CADABRA_STRING);
 
         /* Then. */
         $this->assertEmpty($user);
@@ -54,10 +55,10 @@ class Model_User_Test extends Model_Base_Test
     protected function assertDummyUser($user)
     {
         $this->assertNotEmpty($user);
-        $this->assertEquals('dummy@mail.com', $user->email);
-        $this->assertEquals('0f0a9a777952ceb5b629ec5a901df612c7bf2cd66a63ef2d80228d5557ca8dca', $user->password);
+        $this->assertEquals(self::EMAIL, $user->email);
+        $this->assertEquals(self::HASHED_PASSWORD, $user->password);
         $this->assertTrue($user->enabled == true);
-        $this->assertEquals('Dummy', $user->username);
+        $this->assertEquals(self::USER_NAME, $user->username);
     }
 
 }
